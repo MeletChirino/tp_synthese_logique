c6cd26444670737bc1453c54a855605ad6f5cc16
Melet David <melet.chirino@gmail.com>
2021-11-07 12:26:22 +0100

    [UPDATE] decoder in positive logic

--- Simulation/decoder.vhd
+++ Simulation/decoder.vhd
@@ -11,21 +11,15 @@ end entity;
 architecture rtl of decodeur is
 begin
 	with input select output <=
-		"0000001" when x"0",
-		"1001111" when x"1",
-		"0010010" when x"2",
-		"0000110" when x"3",
-		"1001100" when x"4",
-		"0100100" when x"5",
-		"0100000" when x"6",
-		"0001111" when x"7",
-		"0000000" when x"8",
-		"0001100" when x"9",
-		"0001000" when x"a",
-		"1100000" when x"b",
-		"0110000" when x"c",
-		"1000010" when x"d",
-		"0110001" when x"e",
-		"0111000" when x"f",
+		"1111110" when x"0",
+		"0110000" when x"1",
+		"1101101" when x"2",
+		"1111001" when x"3",
+		"0110011" when x"4",
+		"1011011" when x"5",
+		"1011111" when x"6",
+		"1110000" when x"7",
+		"1111111" when x"8",
+		"1110011" when x"9",
 		"0000000" when others;
 end rtl;
